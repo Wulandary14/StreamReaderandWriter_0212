514907803d53600c5bf5e5581c03f8a15a1ac1e9
Wulandary14 <Moonywulan14@gmail.com>
2026-06-01 19:53:13 +0700

    menutup, membuka, dan menunjuk file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -30,7 +30,18 @@ int main()
         //kondisi jika baris hanya  berisi huruf q
         if (baris == "q") break;
         //menulis dan memasukkan nilai dari baris ke ddalam file
-        outfile << bat]ris << endl;
+        outfile << baris << endl;
 
     }
+    //menutup file apabila telah selesai di tulis
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file denganekstensi .txt
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file" << endl;
+    
 }
